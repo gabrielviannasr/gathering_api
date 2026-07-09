8d95b48a4b038d87a8ae53825f4c7e72f1f43c4c
Gabriel Vianna <gabriel.viannasr@gmail.com>
2026-07-09 18:02:05 -0300

    refactor(round): exclude event from API response

--- src/main/java/br/com/gathering/entity/Round.java
+++ src/main/java/br/com/gathering/entity/Round.java
@@ -3,6 +3,8 @@ package br.com.gathering.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -37,6 +39,7 @@ public class Round {
 	@Column(name = "id_event", nullable = false)
 	private Long idEvent;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "id_event", nullable = true, insertable = false, updatable = false)
 	private Event event;
