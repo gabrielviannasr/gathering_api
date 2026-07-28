09ff2794628081f0a71edc05c1e2796cadf2db12
Gabriel Vianna <gabriel.viannasr@gmail.com>
2026-07-28 17:44:46 -0300

    refactor(service): remove unused imports

--- src/main/java/br/com/gathering/entity/Transaction.java
+++ src/main/java/br/com/gathering/entity/Transaction.java
@@ -14,7 +14,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
--- src/main/java/br/com/gathering/service/GatheringService.java
+++ src/main/java/br/com/gathering/service/GatheringService.java
@@ -1,7 +1,6 @@
 package br.com.gathering.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
